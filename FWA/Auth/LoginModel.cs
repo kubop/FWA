d5ae4d98cac211d6a6deb1ded89263c492cc7a97
Jakub Pernica <jakub.pernica@apsoft.sk>
2023-04-11 16:16:47 +0200

    add login view, controller

---
+++ FWA/Auth/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FWAweb.Auth;
+
+public class LoginModel
+{
+    [DisplayName("Username")]
+    public string Login { get; set; }
+
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
---
+++ FWA/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using FWAweb.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FWAweb.Controllers
+{
+    public class AuthController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login() { return View("Index"); }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([Bind("Login,Password")] LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Login");
+            }
+
+            return RedirectToAction("Index", "User");
+        }
+    }
+}
